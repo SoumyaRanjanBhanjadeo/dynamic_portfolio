9286a5e9a0eb6a7cce1d60fe9a77d7d88de3cba1
Soumya <asish.bhanjadeo@gmail.com>
2024-10-01 10:17:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config();
+const { connect } = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+const portfolioRoute = require("./routes/portfolioRoute");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+
+app.use("/api/portfolio", portfolioRoute);
+
+connect(process.env.MONGO_URI)
+  .then(
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ models/portfolioModel.js
@@ -0,0 +1,121 @@
+const { Schema, model } = require("mongoose");
+
+const introSchema = new Schema({
+  welcomeText: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const aboutSchema = new Schema({
+  lottieURL: {
+    type: String,
+    required: true,
+  },
+  description1: {
+    type: String,
+    required: true,
+  },
+  description2: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: Array,
+    required: true,
+  },
+});
+
+const experienceSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  period: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const projectSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  technologies: {
+    type: Array,
+    required: true,
+  },
+});
+
+const conactSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+
+module.exports = {
+    Intro : model("intros", introSchema),
+    About : model("abouts", aboutSchema),
+    Experience: model("experience", experienceSchema),
+    Project : model("projects", projectSchema),
+    Contact : model("contacts", conactSchema),
+}
